82e73359a608d541940f1ea794904f010c023136
uwi0 <kakapoweb@gmail.com>
2024-08-29 07:47:35 +0700

    Initial Commit

---
+++ Chocolate View/Chocolate_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chocolate_ViewApp.swift
+//  Chocolate View
+//
+//  Created by dwi prasetyo on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Chocolate_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
