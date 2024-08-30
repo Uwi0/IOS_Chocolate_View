d2094b9a7015345e1117296345b187f19ad203c6
uwi0 <kakapoweb@gmail.com>
2024-08-30 10:16:09 +0700

    finishing chocolate view part 1

--- Chocolate View/ContentView.swift
+++ Chocolate View/ContentView.swift
@@ -1,21 +1,41 @@
-//
-//  ContentView.swift
-//  Chocolate View
-//
-//  Created by dwi prasetyo on 29/08/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let textCount = 28
+    private let text: String = "Chocolate "
+    private let n = 300
+    private let spacing = 20.0
+    private let opacity = 0.7
+    
+    @State private var angle = -50.0
+    private var chocolateTexts: String {
+        return String(repeating: text, count: textCount)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: [.blue, .red],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .opacity(0.7)
+            VStack(spacing: spacing) {
+                ForEach(0 ..< n, id: \.self) { _ in
+                    Text(chocolateTexts)
+                        .fixedSize(horizontal: true, vertical: false)
+                        .font(.caption)
+                }
+            }
+            .rotationEffect(Angle(degrees: angle))
+            .opacity(opacity)
         }
-        .padding()
+        .onTapGesture {
+            withAnimation(.linear(duration: 2)) {
+                angle = angle * -1
+            }
+        }
     }
 }
 
